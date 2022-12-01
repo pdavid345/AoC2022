46f4ea18a27b052a65eda35136bc7723a1b1455e
David Panyi <dpanyi@jaguarlandrover.com>
2022-12-01 09:58:07 +0100

    Problem 1 done

--- pr1.py
+++ pr1.py
@@ -7,3 +7,10 @@ for l in dat:
     maxSum = max([(sum([int(i) for i in l.split('\n')])),maxSum])
 
 print(f'Part 1: {maxSum}')
+
+sums = []
+for l in dat:
+    sums.append((sum([int(i) for i in l.split('\n')])))
+
+sums.sort()
+print(f'Part 2: {sum(sums[-3:])}')
