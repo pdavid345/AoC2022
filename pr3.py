60d560b66b700012badfeb96ddb228c599210a7a
David Panyi <dpanyi@jaguarlandrover.com>
2022-12-03 14:09:39 +0100

    corrected printing part 1 instead of part2

--- pr3.py
+++ pr3.py
@@ -6,6 +6,6 @@ lookUp = dict(zip( map(chr,list(range(65,91))[:]+list(range(97,123))[:])  , list
 sol1 = sum([lookUp[set(l[0:int(len(l)/2)]).intersection(set(l[int(len(l)/2):])).pop()] for l in dat])
 sol2 = sum([lookUp[set(x).intersection(set(y)).intersection(set(z)).pop()] for x, y, z in zip(*[iter(dat)]*3)])
 
-print(sol2)
+print(sol1)
 print(sol2)
     
